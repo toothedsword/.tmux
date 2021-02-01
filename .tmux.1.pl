a415c5b2f35f225933bbb77ceb9510e148903d73
toothedsword <liang_peng_chyg@163.com>
2021-02-01 12:12:36 +0800

    add .tmux.1.pl

---
+++ .tmux.1.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+# /**---------------------------------------------------------------------------
+#
+#         File: .tmux.1.pl
+#
+#        Usage: ./.tmux.1.pl  
+#
+#  Description: 
+#
+#      Options: ---
+# Requirements: ---
+#         Bugs: ---
+#        Notes: ---
+#       Author: Liang Peng (...), pengliang@piesat
+# Organization: ...
+#      Version: 1.0
+#      Created: 09/29/2020 12:45:35 PM
+#     Revision: ---
+# ----------------------------------------------------------------------------*/
+
+use strict;
+use warnings;
+use utf8;
+
+my ($sec,$min,$hour,$mday,$mon,$year_off,$wday,$yday,$isdat) = localtime;
+
+my $h1 = $sec % 4;
+$h1++;
+if ($h1 > 4) {
+    $h1 = 1;
+}
+if ($h1 == 1) {
+    print "🌐";
+}
+if ($h1 == 2) {
+    print "🌏";
+}
+if ($h1 == 3) {
+    print "🌍";
+}
+if ($h1 == 4) {
+    print "🌎";
+}
+
+
+
