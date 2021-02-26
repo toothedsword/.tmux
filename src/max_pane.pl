35badab6cdb66381940e0ed8e84caba3d5b783d6
toothedsword <liang_peng_chyg@163.com>
2021-02-27 00:37:33 +0800

    add color change in max pane

--- src/max_pane.pl
+++ src/max_pane.pl
@@ -1,12 +1,17 @@
+my $m = shift;
 my $np=`tmux list-panes | wc -l`;
 chomp($np);
 
 if ($np > 1) {
     system("tmux resize-pane -Z");
+    if ($m) {
+        system("tmux set window-active-style 'fg=6 bg=default'");        
+    } else {
+        system("tmux set window-active-style 'fg=#ff0000 bg=#333333'");        
+    }
 } else {
-# system("tmux send-keys C-b z Enter");
-# system("tmux send-keys C-DoubleClick1Pane Enter");
-system("tmux split-window -v");    
-system("tmux split-window -h");    
+    # system("tmux send-keys C-b z Enter");
+    # system("tmux send-keys C-DoubleClick1Pane Enter");
+    system("tmux split-window -v");    
+    system("tmux split-window -h");    
 }
-
