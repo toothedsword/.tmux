a821ba8b42a5010470293f03e09a72610c031f68
toothedsword <liang_peng_chyg@163.com>
2021-03-23 14:51:45 +0800

    2021年 03月 23日 星期二 14:51:45 CST

---
+++ src/check.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+# /**---------------------------------------------------------------------------
+#
+#         File: check.pl
+#
+#        Usage: ./check.pl  
+#
+#  Description: 
+#
+#      Options: ---
+# Requirements: ---
+#         Bugs: ---
+#        Notes: ---
+#       Author: Liang Peng (...), pengliang@piesat
+# Organization: ...
+#      Version: 1.0
+#      Created: 2021年03月22日 17时46分56秒
+#     Revision: ---
+# ----------------------------------------------------------------------------*/
+
+use strict;
+use warnings;
+use utf8;
+
+
+while(1) {
+    
+    sleep(5);
+}
+
