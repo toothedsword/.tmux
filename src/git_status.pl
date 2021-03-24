f8d0e89ea5f4c4339a7db63406f99d50a9aa0cda
toothedsword <liang_peng_chyg@163.com>
2021-03-24 10:46:07 +0800

    2021年 03月 24日 星期三 10:46:07 CST

---
+++ src/git_status.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+# /**---------------------------------------------------------------------------
+#
+#         File: git_status.pl
+#
+#        Usage: ./git_status.pl  
+#
+#  Description: 
+#
+#      Options: ---
+# Requirements: ---
+#         Bugs: ---
+#        Notes: ---
+#       Author: Liang Peng (...), pengliang@piesat
+# Organization: ...
+#      Version: 1.0
+#      Created: 2021年03月21日 16时45分23秒
+#     Revision: ---
+# ----------------------------------------------------------------------------*/
+
+use strict;
+use warnings;
+use utf8;
+
+
+
